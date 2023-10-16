d3ddff0baea5a965f0e929f7189663439bb00224
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-10-16 19:46:02 -0300

    EnderecamentoAberto

--- src/TabelaHash/EnderecamentoAberto/EnderecamentoAberto.java
+++ src/TabelaHash/EnderecamentoAberto/EnderecamentoAberto.java
@@ -1,4 +1,65 @@
 package TabelaHash.EnderecamentoAberto;
+import TabelaHash.EstruturaHash;
+import TabelaHash.Jogador;
+import java.util.ArrayList;
+import java.util.List;
 
-public class EnderecamentoAberto {
+public class EnderecamentoAberto extends EstruturaHash {
+    private int tamanho; // tamanho da tabela
+    private Jogador[] time; // vetor de jogadores
+
+    public EnderecamentoAberto(int tamanho) {
+        super(tamanho);
+        this.tamanho = tamanho;
+        this.time = new Jogador[tamanho];
+    }
+
+    public void inserir(Jogador jogador) {
+        int chave = jogador.getNumeroCamisa();
+        int indice = funcaoHash(chave);
+
+        while (time[indice] != null) {
+            // Implementação da estratégia de sondagem (neste caso, sondagem linear)
+            indice = (indice + 1) % tamanho;
+        }
+
+        time[indice] = jogador;
+    }
+
+    public Jogador buscar(int chave) {
+        int indice = funcaoHash(chave);
+        while (time[indice] != null) {
+            if (time[indice].getNumeroCamisa() == chave) {
+                return time[indice];
+            }
+            indice = (indice + 1) % tamanho;
+        }
+        return null;
+    }
+
+    public Jogador remover(int chave) {
+        int indice = funcaoHash(chave);
+        while (time[indice] != null) {
+            if (time[indice].getNumeroCamisa() == chave) {
+                Jogador jogadorRemovido = time[indice];
+                time[indice] = null;
+                return jogadorRemovido;
+            }
+            indice = (indice + 1) % tamanho;
+        }
+        return null;
+    }
+
+    public void exibirTabela() {
+        System.out.println("===================================");
+        System.out.println("Tabela Hash de Endereçamento Aberto:");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print("Slot " + i + ": ");
+            if (time[i] != null) {
+                System.out.print(time[i].getNome() + " ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println("===================================");
+    }
 }
--- src/TabelaHash/EnderecamentoAberto/Teste.java
+++ src/TabelaHash/EnderecamentoAberto/Teste.java
@@ -1,4 +1,42 @@
 package TabelaHash.EnderecamentoAberto;
 
+import TabelaHash.Jogador;
+
 public class Teste {
+    public static void main(String[] args) {
+        int tamanhoTabela = 10; // Defina o tamanho da tabela hash
+
+        EnderecamentoAberto tabela = new EnderecamentoAberto(tamanhoTabela);
+
+        // Inserindo jogadores na tabela
+        tabela.inserir(new Jogador(10, "Ney"));
+        tabela.inserir(new Jogador(11, "Rodrygo"));
+        tabela.inserir(new Jogador(7, "Vini Jr"));
+        tabela.inserir(new Jogador(0, "Bruno"));
+
+
+        // Exibindo a tabela
+        tabela.exibirTabela();
+
+        // Buscando um jogador por chave
+        int chaveBusca = 10;
+        Jogador jogadorEncontrado = tabela.buscar(chaveBusca);
+        if (jogadorEncontrado != null) {
+            System.out.println("Jogador encontrado: " + jogadorEncontrado.getNome());
+        } else {
+            System.out.println("Jogador não encontrado para a chave " + chaveBusca);
+        }
+
+        // Removendo um jogador por chave
+        int chaveRemover = 20;
+        Jogador jogadorRemovido = tabela.remover(chaveRemover);
+        if (jogadorRemovido != null) {
+            System.out.println("Jogador removido: " + jogadorRemovido.getNome());
+        } else {
+            System.out.println("Jogador não encontrado para remover com chave " + chaveRemover);
+        }
+
+        // Exibindo a tabela após a remoção
+        tabela.exibirTabela();
+    }
 }
